a73fd2504f8404dbb21b54f298b5b8b0fd275424
David Rehmat <me@dave.blue>
2024-05-27 14:13:30 -0400

    make this a little more specific

--- mterm.js
+++ mterm.js
@@ -1,6 +1,6 @@
 module.exports = {
     RUNNER_THEME_CSS: `
-        .runner-container {
+        .runner-container.runner-container__normal_mode, .runner-container.runner-container__commander_mode {
           background: rgba(199, 0, 20, 0.89);
           color: white;
         }
